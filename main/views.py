b0467d02f6c774d35511cef22145fc8e406ca6fa
Miika Nissi <miika@miikanissi.com>
2022-09-25 19:50:10 -0400

    Combine Art and Writing models into Content model
    Add masonry to card list views

--- main/views.py
+++ main/views.py
@@ -3,14 +3,12 @@ from django.shortcuts import get_object_or_404
 from django.shortcuts import render
 from taggit.models import Tag
 
-from .models import Art
-from .models import Writing
+from .models import Content
 
 
 def index(request):
-    arts = Art.objects.filter(status=1).order_by("-created_on")[:6]
-    writings = Writing.objects.filter(status=1).order_by("-created_on")[:6]
-    return render(request, "index.html", {"arts": arts, "writings": writings})
+    contents = Content.objects.filter(status=1).order_by("-created_on")[:12]
+    return render(request, "index.html", {"contents": contents})
 
 
 def about(request):
@@ -21,43 +19,29 @@ def contact(request):
     return render(request, "contact.html")
 
 
-def writing_detail(request, slug):
-    template_name = "writing_detail.html"
-    writing = get_object_or_404(Writing, slug=slug)
-    return render(request, template_name, {"writing": writing})
-
-
-def writing_list(request, tag_slug=None):
-    template_name = "writing_list.html"
+def content_list(request, category=None, tag_slug=None):
+    template_name = "content_list.html"
     tag = None
-    writings = Writing.objects.filter(status=1).order_by("-created_on")
-    paginator = Paginator(writings, 6)
+    if category:
+        contents = Content.objects.filter(status=1, category=category).order_by(
+            "-created_on"
+        )
+    else:
+        contents = Content.objects.filter(status=1).order_by("-created_on")
+    paginator = Paginator(contents, 12)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
     if tag_slug:
         tag = get_object_or_404(Tag, slug=tag_slug)
-        writings = writings.filter(tags__in=[tag])
+        contents = contents.filter(tags__in=[tag])
     return render(
-        request, template_name, {"writings": writings, "page_obj": page_obj, "tag": tag}
+        request,
+        template_name,
+        {"contents": contents, "category": category, "page_obj": page_obj, "tag": tag},
     )
 
 
-def art_detail(request, slug):
-    template_name = "art_detail.html"
-    art = get_object_or_404(Art, slug=slug)
-    return render(request, template_name, {"art": art})
-
-
-def art_list(request, tag_slug=None):
-    template_name = "art_list.html"
-    tag = None
-    arts = Art.objects.filter(status=1).order_by("-created_on")
-    paginator = Paginator(arts, 6)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
-    if tag_slug:
-        tag = get_object_or_404(Tag, slug=tag_slug)
-        arts = arts.filter(tags__in=[tag])
-    return render(
-        request, template_name, {"arts": arts, "page_obj": page_obj, "tag": tag}
-    )
+def content_detail(request, slug, category=None):
+    template_name = "content_detail.html"
+    content = get_object_or_404(Content, slug=slug)
+    return render(request, template_name, {"content": content, "category": category})

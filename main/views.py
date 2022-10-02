ac0c7c002e7cf2bb00e3dad499584180a148899e
Miika Nissi <miika@miikanissi.com>
2022-10-02 19:04:44 -0400

    Create contact page with form
    Add font and some style rules

--- main/views.py
+++ main/views.py
@@ -1,9 +1,20 @@
+import environ
+from django.contrib import messages
+from django.core.mail import BadHeaderError
+from django.core.mail import EmailMessage
 from django.core.paginator import Paginator
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render
+from django.urls import reverse
 from taggit.models import Tag
 
+from .forms import ContactForm
 from .models import Content
+
+env = environ.Env(
+    EMAIL_FROM=(str, "example@example.com"), EMAIL_TO=(str, "example@example.com")
+)
 
 
 def index(request):
@@ -16,7 +27,38 @@ def about(request):
 
 
 def contact(request):
-    return render(request, "contact.html")
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            subject = "Website Inquiry - %s" % form.cleaned_data["name"]
+            body = {
+                "name": form.cleaned_data["name"],
+                "email": form.cleaned_data["email"],
+                "message": form.cleaned_data["message"],
+            }
+            message = "\n".join(body.values())
+            email = EmailMessage(
+                subject,
+                message,
+                env("EMAIL_FROM"),
+                [env("EMAIL_TO")],
+                reply_to=[form.cleaned_data["email"]],
+            )
+            try:
+                email.send()
+                messages.success(
+                    request,
+                    "Your message was sent, I will get back to you shortly, thank you!",
+                )
+                return HttpResponseRedirect(reverse(contact))
+            except BadHeaderError:
+                messages.error(
+                    request,
+                    "Something went wrong. Please try again.",
+                )
+    else:
+        form = ContactForm()
+    return render(request, "contact.html", {"form": form})
 
 
 def content_list(request, category=None, tag_slug=None):

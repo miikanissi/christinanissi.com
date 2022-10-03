9edf7685f20aba34a2ab2b65d99d7ac9f808ee0f
Miika Nissi <miika@miikanissi.com>
2022-10-02 22:19:25 -0400

    Improve static file and favicon delivery
    Add content to about page. Small style detailing.

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,7 @@
 """The main application's URLs."""
+from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import path
+from django.views.generic.base import RedirectView
 
 from . import views
 from .feeds import AtomFeed
@@ -8,12 +10,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("content", views.content_list, name="content_list"),
     path("writing", views.content_list, {"category": 2}, name="writing_list"),
-    path(
-        "writing/tag/<slug:tag_slug>/",
-        views.content_list,
-        {"category": 2},
-        name="writing_tag",
-    ),
+    path("tag/<slug:tag_slug>/", views.content_list, name="tag"),
     path(
         "writing/<slug:slug>/",
         views.content_detail,
@@ -21,21 +18,54 @@ urlpatterns = [
         name="writing_detail",
     ),
     path("art", views.content_list, {"category": 1}, name="art_list"),
-    path(
-        "art/tag/<slug:tag_slug>/", views.content_list, {"category": 1}, name="art_tag"
-    ),
     path("art/<slug:slug>/", views.content_detail, {"category": 1}, name="art_detail"),
     path("about/", views.about, name="about"),
     path("contact/", views.contact, name="contact"),
     path("rss", AtomFeed()),
-    path("favicon-32x32.png", views.favicon_32),
-    path("favicon-16x16.png", views.favicon_16),
-    path("favicon.ico", views.favicon),
-    path("apple-touch-icon.png", views.apple_touch_icon),
-    path("site.webmanifest", views.manifest),
-    path("safari-pinned-tab.svg", views.mask_icon),
-    path("android-chrome-192x192.png", views.android_chrome_192),
-    path("android-chrome-512x512.png", views.android_chrome_512),
-    path("browserconfig.xml", views.browserconfig),
-    path("mstile-150x150.png", views.mstile),
+    path(
+        "favicon-32x32.png",
+        RedirectView.as_view(url=staticfiles_storage.url("files/favicon-32x32.png")),
+    ),
+    path(
+        "favicon-16x16.png",
+        RedirectView.as_view(url=staticfiles_storage.url("files/favicon-16x16.png")),
+    ),
+    path(
+        "favicon.ico",
+        RedirectView.as_view(url=staticfiles_storage.url("files/favicon.ico")),
+    ),
+    path(
+        "apple-touch-icon.png",
+        RedirectView.as_view(url=staticfiles_storage.url("files/apple-touch-icon.png")),
+    ),
+    path(
+        "site.webmanifest",
+        RedirectView.as_view(url=staticfiles_storage.url("files/site.webmanifest")),
+    ),
+    path(
+        "safari-pinned-tab.svg",
+        RedirectView.as_view(
+            url=staticfiles_storage.url("files/safari-pinned-tab.svg")
+        ),
+    ),
+    path(
+        "android-chrome-192x192.png",
+        RedirectView.as_view(
+            url=staticfiles_storage.url("files/android-chrome-192x192.png")
+        ),
+    ),
+    path(
+        "android-chrome-512x512.png",
+        RedirectView.as_view(
+            url=staticfiles_storage.url("files/android-chrome-512x512.png")
+        ),
+    ),
+    path(
+        "browserconfig.xml",
+        RedirectView.as_view(url=staticfiles_storage.url("files/browserconfig.xml")),
+    ),
+    path(
+        "mstile-150x150.png",
+        RedirectView.as_view(url=staticfiles_storage.url("files/mstile-150x150.png")),
+    ),
 ]

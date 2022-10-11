c22e131a3d131628ff48b1ca2d3f648cfb6a11c1
Miika Nissi <miika@miikanissi.com>
2022-10-11 15:34:24 -0400

    Improve feed generation

--- src/main/feeds.py
+++ src/main/feeds.py
@@ -6,9 +6,10 @@ from .models import Content
 
 
 class LatestFeed(Feed):
-    title = "Christina Nissi"
-    link = "https://christinanissi.com"
-    description = "New content on my website."
+    title = "Christina Nissi's Website Feed"
+    author_name = "Christina Nissi"
+    link = "https://christinanissi.com/"
+    description = "New content posted on Christina Nissi's website."
 
     def items(self):
         content = Content.objects.filter(status=1, publish_date__lt=timezone.now())
@@ -23,6 +24,19 @@ class LatestFeed(Feed):
 
     def item_link(self, item):
         return item.get_absolute_url()
+
+    def item_updateddate(self, item):
+        return item.update_date
+
+    def item_pubdate(self, item):
+        return item.publish_date
+
+    def item_categories(self, item):
+        if item.category == 1:
+            return ("Art",)
+        if item.category == 2:
+            return ("Writing",)
+        return ("Undefined",)
 
 
 class AtomFeed(LatestFeed):
